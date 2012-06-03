c940ce4fe18c6c5df2a7ab70d6fa74cd6dd28212
ricbaral <ricbaral@gmail.com>
2012-06-03 09:37:00 +0000

    changed global theme main ist jetzt startbildschirm

--- energiespar-app/src/de/hska/rbmk/geraetevergleich/GeraetevergleichActivtiy.java
+++ energiespar-app/src/de/hska/rbmk/geraetevergleich/GeraetevergleichActivtiy.java
@@ -9,6 +9,6 @@ public class GeraetevergleichActivtiy extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.startbildschirm);
     }
 }
