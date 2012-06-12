7f548a3673af1c2106687c5e31221801cf9cda03
ricbaral <ricbaral@gmail.com>
2012-06-12 19:27:53 +0000

    rechner programmierung

---
+++ energiespar-app/src/de/hska/rbmk/verbrauchsrechner/SpuelmaschinenActivity.java
@@ -0,0 +1,52 @@
+package de.hska.rbmk.verbrauchsrechner;
+
+import de.hska.rbmk.R;
+import de.hska.rbmk.StartbildschirmActivity;
+import de.hska.rbmk.ThemedActivity;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class SpuelmaschinenActivity extends ThemedActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.rechner_waschmaschine);
+		
+		String[] geraeteListe = getResources().getStringArray(R.array.GeraeteListe);
+	    ActionBar actionBar = getActionBar();
+	    actionBar.setDisplayHomeAsUpEnabled(true);
+	    actionBar.setTitle(geraeteListe[2]); // 0 = Kühlschrank, 1 = Waschmaschine, 2 = Spülmaschine
+	    actionBar.setIcon(R.drawable.ic_calc_dishw);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+
+		{
+			MenuInflater inflater = new MenuInflater(this);
+			inflater.inflate(R.menu.rechner_typ_auswahl, menu);
+		}
+
+		return true;
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	            // app icon in action bar clicked; go home
+	            Intent intent = new Intent(this, StartbildschirmActivity.class);
+	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	            startActivity(intent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+}
