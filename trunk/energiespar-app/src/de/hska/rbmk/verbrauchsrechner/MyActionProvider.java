26808783f6a538f961190fa8e413be94d14f1c9d
ricbaral@gmail.com <ricbaral@gmail.com@be0d9dae-6986-2689-6b11-b4d497674b0b>
2012-06-12 21:50:42 +0000

    Implementierung des Vergleichsrechners 30% +activities +layouts +icons

--- trunk/energiespar-app/src/de/hska/rbmk/verbrauchsrechner/MyActionProvider.java
+++ trunk/energiespar-app/src/de/hska/rbmk/verbrauchsrechner/MyActionProvider.java
@@ -1,25 +1,15 @@
 package de.hska.rbmk.verbrauchsrechner;
 
 
-import java.util.List;
-
 import de.hska.rbmk.R;
-import de.hska.rbmk.StartbildschirmActivity;
-import de.hska.rbmk.zaehlerstand.ZaehlerstandErfassenActivity;
-
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.util.Log;
 import android.view.ActionProvider;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.SubMenu;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MyActionProvider extends ActionProvider implements OnMenuItemClickListener {
 
@@ -34,8 +24,6 @@ public class MyActionProvider extends ActionProvider implements OnMenuItemClickL
 
 	@Override
 	public View onCreateActionView() {
-		Log.d(this.getClass().getSimpleName(), "onCreateActionView");
-
 		TextView textView = new TextView(mContext);
 		textView.setText("Pick");
 
@@ -44,42 +32,40 @@ public class MyActionProvider extends ActionProvider implements OnMenuItemClickL
 
 	@Override
 	public boolean onPerformDefaultAction() {
-		Log.d(this.getClass().getSimpleName(), "onPerformDefaultAction");
-
 		return super.onPerformDefaultAction();
 	}
 
 	@Override
 	public boolean hasSubMenu() {
-		Log.d(this.getClass().getSimpleName(), "hasSubMenu");
-
 		return true;
 	}
 
 	@Override
 	public void onPrepareSubMenu(SubMenu subMenu) {
-		Log.d(this.getClass().getSimpleName(), "onPrepareSubMenu");
-
 		subMenu.clear();
 
 		String[] geraeteListe = mContext.getResources().getStringArray(R.array.GeraeteListe);
 		String[] geraeteListeSymbole = mContext.getResources().getStringArray(R.array.GeraeteListeSymbole);
 		
-		// TODO schlechte implementation 
+		// TODO schlechte submenu implementation 
+		
+		// Kühlschränke 
 		subMenu.add(0, 0, 0, geraeteListe[0])
 		.setIcon(mContext.getResources().getIdentifier(geraeteListeSymbole[0], "drawable", mContext.getPackageName()))
 		.setOnMenuItemClickListener(this)
-		.setIntent(new Intent(mContext, StartbildschirmActivity.class));
+		.setIntent(new Intent(mContext, KuehlschraenkeActivity.class));
 		
+		// Waschmaschinen
 		subMenu.add(0, 1, 1, geraeteListe[1])
 		.setIcon(mContext.getResources().getIdentifier(geraeteListeSymbole[1], "drawable", mContext.getPackageName()))
 		.setOnMenuItemClickListener(this)
-		.setIntent(new Intent(mContext, StartbildschirmActivity.class));
+		.setIntent(new Intent(mContext, WaschmaschinenActivity.class));
 		
+		// Spülmaschinen
 		subMenu.add(0, 2, 2, geraeteListe[2])
 		.setIcon(mContext.getResources().getIdentifier(geraeteListeSymbole[2], "drawable", mContext.getPackageName()))
 		.setOnMenuItemClickListener(this)
-		.setIntent(new Intent(mContext, StartbildschirmActivity.class));
+		.setIntent(new Intent(mContext, SpuelmaschinenActivity.class));
 	}
 
 	@Override
