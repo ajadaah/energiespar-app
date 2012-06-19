ea65d11cc4a5deb1aa3182f8304fcea461f49a39
ricbaral <ricbaral@gmail.com>
2012-06-19 18:47:13 +0000

    Neue Permission: RECEIVE_BOOT_COMPLETED Die Erinnerung funktioniert jetzt auch nach Reboot des OS

--- energiespar-app/src/de/hska/rbmk/zaehlerstand/AlarmService.java
+++ energiespar-app/src/de/hska/rbmk/zaehlerstand/AlarmService.java
@@ -11,6 +11,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class AlarmService extends Service {
 	
@@ -35,7 +36,6 @@ public class AlarmService extends Service {
 		editor = getSharedPreferences(Constants.SHARED_PREFERENCES,MODE_PRIVATE).edit();
 		
 		sucheNachErinnerung();
-		
 		// TODO: evtl immer nach 24h checken?
 		
 		return startId;
